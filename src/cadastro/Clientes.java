6e87c2929fb7a72400097419d6fa20e5c094f5f8
Guilherme Francisco Dos Santos Silva <silva.gui22g@gmail.com>
2023-10-16 19:44:56 -0300

    Created new project Java

---
+++ src/cadastro/Clientes.java
@@ -0,0 +1,10 @@
+package cadastro;
+
+public class Clientes extends Pessoas{
+    public Clientes() {
+    }
+
+    public Clientes(String nome, String email, String cpf, String endereco) {
+        super(nome, email, cpf, endereco);
+    }
+}
---
+++ src/cadastro/Produtos.java
@@ -0,0 +1,10 @@
+package cadastro;
+
+public class Produtos extends Pessoas {
+    public Produtos() {
+    }
+
+    public Produtos(String nome, String email, String cpf, String endereco) {
+        super(nome, email, cpf, endereco);
+    }
+}
